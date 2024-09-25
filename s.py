adb07c94ab23fe4bb2b419d90add068aa2036098
Persia Shahdi <persiashahdi@gmail.com>
2024-09-25 18:36:23 -0400

    added adder

--- s.py
+++ s.py
@@ -1,7 +1,10 @@
 def multiply(a, b):
     return a * b
 
+def add(a, b):
+    return a + b
+
 # Test the corrected method
-print(multiply(3, 5))
+print(add(3, 5))
 print(multiply(-1, 1))
 print(multiply(0, 0)) 
